39a5b832df4c2ffa285bc7c0744077f606916f69
Vui Seng Chua <vui.seng.chua@intel.com>
2022-09-17 19:49:53 -0700

    Revise qat config and launcher

---
+++ examples/pytorch/audio-classification/qat-noignore-w2v2-b-ac.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+
+export WANDB_DISABLED=false # Enable wandb
+export WANDB_WATCH=false # Disable gradient serialization to wandb
+export WANDB_USERNAME=vchua
+export WANDB_API_KEY=f8a95080288950342f1695008cd8256adc3b0778
+
+# ---------------------------------------------------------------------------------------------
+export WANDB_PROJECT="w2v2opt-ks (dgx4)"
+export CUDA_VISIBLE_DEVICES=0
+
+NEPOCH=5
+BS=64
+GA=2
+LR=3e-5
+RUNID=qat-ft-noignore-w2v2-b-ks-bs${BS}-${NEPOCH}eph-$LR
+
+NNCFCFG=/data2/vchua/dev/w2v2-ion/transformers/examples/pytorch/audio-classification/nncfcfg/qat-noignore-w2v2-b.json
+OUTROOT=/data1/vchua/run/w2v2-ion/w2v2b-ks/
+
+WORKDIR=/data2/vchua/dev/w2v2-ion/transformers/examples/pytorch/audio-classification
+
+CONDAROOT=/data1/vchua/miniconda3
+CONDAENV=w2v2-ion
+# ---------------------------------------------------------------------------------------------
+
+OUTDIR=$OUTROOT/$RUNID
+
+# override label if in dryrun mode
+if [[ $1 == "dryrun" ]]; then
+    OUTDIR=$OUTROOT/dryrun-${RUNID}
+    RUNID=dryrun-${RUNID}
+fi
+
+mkdir -p $OUTDIR
+cd $WORKDIR
+    # --lr_scheduler_type cosine_with_restarts \
+    # --distill_temp $TEMPERATURE \
+    # --load_best_model_at_end True \
+    # --teacher anton-l/wav2vec2-base-ft-keyword-spotting \
+    # --teacher_ratio $ALPHA \
+    # --warmup_ratio 0.04 \
+
+cmd="
+python run_audio_classification.py \
+    --model_name_or_path facebook/wav2vec2-base \
+    --dataset_name superb \
+    --dataset_config_name ks \
+    --remove_unused_columns False \
+    --do_eval \
+    --do_train \
+    --nncf_config $NNCFCFG \
+    --learning_rate $LR \
+    --max_length_seconds 1 \
+    --attention_mask False \
+    --num_train_epochs $NEPOCH \
+    --per_device_train_batch_size $BS \
+    --gradient_accumulation_steps 2 \
+    --per_device_eval_batch_size $BS \
+    --dataloader_num_workers 8 \
+    --logging_strategy steps \
+    --logging_steps 1 \
+    --evaluation_strategy epoch \
+    --save_strategy epoch \
+    --seed 0 \
+    --run_name $RUNID \
+    --output_dir $OUTDIR \
+    --overwrite_output_dir
+"
+    # --save_total_limit 3 \
+
+if [[ $1 == "local" ]]; then
+    echo "${cmd}" > $OUTDIR/run.log
+    echo "### End of CMD ---" >> $OUTDIR/run.log
+    cmd="nohup ${cmd}"
+    eval $cmd >> $OUTDIR/run.log 2>&1 &
+    echo "logpath: $OUTDIR/run.log"
+elif [[ $1 == "dryrun" ]]; then
+    echo "[INFO: dryrun, add --max_steps 25 to cli"
+    cmd="${cmd} --max_steps 25"
+    echo "${cmd}" > $OUTDIR/dryrun.log
+    echo "### End of CMD ---" >> $OUTDIR/dryrun.log
+    eval $cmd >> $OUTDIR/dryrun.log 2>&1 &
+    echo "logpath: $OUTDIR/dryrun.log"
+else
+    source $CONDAROOT/etc/profile.d/conda.sh
+    conda activate ${CONDAENV}
+    eval $cmd
+fi
--- examples/pytorch/audio-classification/qat-w2v2-b-ks.sh
+++ examples/pytorch/audio-classification/qat-w2v2-b-ks.sh
@@ -6,21 +6,21 @@ export WANDB_USERNAME=vchua
 export WANDB_API_KEY=f8a95080288950342f1695008cd8256adc3b0778
 
 # ---------------------------------------------------------------------------------------------
-export WANDB_PROJECT="w2v2opt-ks ($(hostname))"
+export WANDB_PROJECT="w2v2opt-ks (dgx4)"
 export CUDA_VISIBLE_DEVICES=0
 
-NEPOCH=12
-TEMPERATURE=2
-ALPHA=0.9
-RUNID=run33.6-w2v2b-ks-mvmt-bt-${NEPOCH}eph-warmup1-6-r0.10-t${TEMPERATURE}-alpha${ALPHA}
-NNCFCFG=/nvme1/vchua/dev/p3-w2v2/transformers/examples/pytorch/audio-classification/cfg-nncf/nncf_w2v2b_ks_mvmt.json
-OUTROOT=/nvme1/vchua/run/p3-w2v2/w2v2b-ks/nncf-mvmt
+NEPOCH=5
+BS=64
+GA=2
+LR=3e-5
+RUNID=qat-ft-txblk-only-w2v2-b-ks-bs${BS}-${NEPOCH}eph-$LR
 
+NNCFCFG=/data2/vchua/dev/w2v2-ion/transformers/examples/pytorch/audio-classification/nncfcfg/qat-txblk-only-w2v2-b-ks.json
+OUTROOT=/data1/vchua/run/w2v2-ion/w2v2b-ks/
 
-WORKDIR=/nvme1/vchua/dev/p3-w2v2/transformers/examples/pytorch/audio-classification
-
-CONDAROOT=/nvme1/vchua/miniconda3
-CONDAENV=p3-w2v2
+WORKDIR=/data2/vchua/dev/w2v2-ion/transformers/examples/pytorch/audio-classification
+CONDAROOT=/data1/vchua/miniconda3
+CONDAENV=w2v2-ion
 # ---------------------------------------------------------------------------------------------
 
 OUTDIR=$OUTROOT/$RUNID
@@ -34,41 +34,39 @@ fi
 mkdir -p $OUTDIR
 cd $WORKDIR
     # --lr_scheduler_type cosine_with_restarts \
+    # --distill_temp $TEMPERATURE \
+    # --load_best_model_at_end True \
+    # --teacher anton-l/wav2vec2-base-ft-keyword-spotting \
+    # --model_name_or_path facebook/wav2vec2-base \
+    # --save_total_limit 3 \
+    # --teacher_ratio $ALPHA \
 
 cmd="
 python run_audio_classification.py \
-    --teacher anton-l/wav2vec2-base-ft-keyword-spotting \
-    --teacher_ratio $ALPHA \
-    --distill_temp $TEMPERATURE \
-    --model_name_or_path anton-l/wav2vec2-base-ft-keyword-spotting \
+    --model_name_or_path facebook/wav2vec2-base \
     --dataset_name superb \
     --dataset_config_name ks \
     --remove_unused_columns False \
     --do_eval \
     --do_train \
     --nncf_config $NNCFCFG \
-    --learning_rate 3e-5 \
+    --learning_rate $LR \
     --max_length_seconds 1 \
     --attention_mask False \
-    --warmup_ratio 0.1 \
     --num_train_epochs $NEPOCH \
-    --per_device_train_batch_size 32 \
-    --gradient_accumulation_steps 4 \
-    --per_device_eval_batch_size 32 \
-    --dataloader_num_workers 4 \
+    --per_device_train_batch_size $BS \
+    --gradient_accumulation_steps $GA \
+    --per_device_eval_batch_size $BS \
+    --dataloader_num_workers 8 \
     --logging_strategy steps \
     --logging_steps 1 \
-    --evaluation_strategy steps \
-    --eval_steps 100 \
-    --save_steps 200 \
-    --load_best_model_at_end True \
-    --metric_for_best_model accuracy \
+    --evaluation_strategy epoch \
+    --save_strategy epoch \
     --seed 0 \
     --run_name $RUNID \
     --output_dir $OUTDIR \
     --overwrite_output_dir
 "
-    # --save_total_limit 3 \
 
 if [[ $1 == "local" ]]; then
     echo "${cmd}" > $OUTDIR/run.log
